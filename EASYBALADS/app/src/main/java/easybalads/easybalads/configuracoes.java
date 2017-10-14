898cd2280b96cf2f79b03be21852e4b25cfe8b9b
LASSDS <leo_anjos1@hotmail.com>
2017-10-14 02:14:54 -0300

    Modificações tela de configurações
    Adicionado função no botão de voltar e modificado configurações do
    layout

--- EASYBALADS/app/src/main/java/easybalads/easybalads/configuracoes.java
+++ EASYBALADS/app/src/main/java/easybalads/easybalads/configuracoes.java
@@ -3,6 +3,7 @@ package easybalads.easybalads;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,6 +21,8 @@ public class configuracoes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
         lvOpcoes = (ListView) findViewById(R.id.lvopcoes);
         opcoes = new ArrayList<String>();
 
@@ -53,4 +56,23 @@ public class configuracoes extends AppCompatActivity {
         Intent it = new Intent(this, termos_de_uso.class);
         startActivity(it);
     }*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, MainActivity.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finish();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){ //Botão BACK padrão do android
+        startActivity(new Intent(this, MainActivity.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        finish(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
+    }
 }
