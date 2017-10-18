9084b232e483d5c80b1721dfdb1e4c0e1f188a4d
LASSDS <leo_anjos1@hotmail.com>
2017-10-18 01:36:53 -0300

    Tela de configurações pronta para testes

--- EASYBALADS/app/src/main/java/easybalads/easybalads/configuracoes.java
+++ EASYBALADS/app/src/main/java/easybalads/easybalads/configuracoes.java
@@ -1,13 +1,20 @@
 package easybalads.easybalads;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,13 +44,16 @@ public class configuracoes extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 switch (position){
                     case 0:
+                        cliqueNotificações(view);
                         break;
                     case 1:
-                        //termos();
+                        cliqueTermos(view);
                         break;
                     case 2:
+                        cliqueAjuda(view);
                         break;
                     case 3:
+                        cliqueSobre(view);
                         break;
                     default:
                         finish();
@@ -52,10 +62,6 @@ public class configuracoes extends AppCompatActivity {
             }
         });
     }
-    /*private void termos(){
-        Intent it = new Intent(this, termos_de_uso.class);
-        startActivity(it);
-    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
@@ -74,5 +80,152 @@ public class configuracoes extends AppCompatActivity {
         startActivity(new Intent(this, MainActivity.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
         finish(); //Método para matar a activity e não deixa-lá indexada na pilhagem
         return;
+    }
+
+    public void cliqueNotificações(View view){
+        //Cria o construtuor do AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //define o titulo
+        builder.setTitle("Notificações");
+        //define a mensagem
+        builder.setMessage("Notificações estão Ativadas!");
+        /*builder.setIcon(R.drawable.desafiosdeti);*/
+        //define um botão positivo
+        builder.setPositiveButton("Ativar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                /*TextView message = (TextView) alerta.findViewById(android.R.id.message);
+                message.setText("Notificações estão Ativadas!");*/
+            }
+        });
+        //define um botão negativo
+        builder.setNegativeButton("Desativar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                /*TextView message = (TextView) alerta.findViewById(android.R.id.message);
+                message.setText("Notificações estão Desativadas!");*/
+            }
+        });
+        //define um botão neutro
+        /*builder.setNeutralButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(MainActivity.this, "Nem Sim, nem Não!", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+        //cria o AlertDialog
+        AlertDialog  alerta = builder.create();
+        //Exibe
+        alerta.show();
+        Button positiveButton = alerta.getButton(AlertDialog.BUTTON_POSITIVE);
+        LinearLayout parent = (LinearLayout) positiveButton.getParent();
+        parent.setGravity(Gravity.CENTER_HORIZONTAL);
+        View leftSpacer = parent.getChildAt(1);
+        leftSpacer.setVisibility(View.GONE);
+    }
+
+    public void cliqueTermos(View view){
+        //Cria o construtuor do AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //define o titulo
+        builder.setTitle("Termos de Uso");
+        //define a mensagem
+        builder.setMessage("Em criação!");
+        /*builder.setIcon(R.drawable.desafiosdeti);*/
+        //define um botão positivo
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+            }
+        });
+        //define um botão negativo
+        /*builder.setNegativeButton("Negativo", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(MainActivity.this, "Vamos Melhorar!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //define um botão neutro
+        builder.setNeutralButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(MainActivity.this, "Nem Sim, nem Não!", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+        //cria o AlertDialog
+        AlertDialog  alerta = builder.create();
+        //Exibe
+        alerta.show();
+        Button positiveButton = alerta.getButton(AlertDialog.BUTTON_POSITIVE);
+        LinearLayout parent = (LinearLayout) positiveButton.getParent();
+        parent.setGravity(Gravity.CENTER_HORIZONTAL);
+        View leftSpacer = parent.getChildAt(1);
+        leftSpacer.setVisibility(View.GONE);
+    }
+
+    public void cliqueAjuda(View view){
+        //Cria o construtuor do AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //define o titulo
+        builder.setTitle("Ajuda");
+        //define a mensagem
+        builder.setMessage("Em criação!");
+        /*builder.setIcon(R.drawable.desafiosdeti);*/
+        //define um botão positivo
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+            }
+        });
+        //define um botão negativo
+        /*builder.setNegativeButton("Negativo", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(MainActivity.this, "Vamos Melhorar!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //define um botão neutro
+        builder.setNeutralButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(MainActivity.this, "Nem Sim, nem Não!", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+        //cria o AlertDialog
+        AlertDialog  alerta = builder.create();
+        //Exibe
+        alerta.show();
+        Button positiveButton = alerta.getButton(AlertDialog.BUTTON_POSITIVE);
+        LinearLayout parent = (LinearLayout) positiveButton.getParent();
+        parent.setGravity(Gravity.CENTER_HORIZONTAL);
+        View leftSpacer = parent.getChildAt(1);
+        leftSpacer.setVisibility(View.GONE);
+    }
+
+    public void cliqueSobre(View view){
+        //Cria o construtuor do AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //define o titulo
+        builder.setTitle("Sobre Nós");
+        //define a mensagem
+        builder.setMessage("Em criação!");
+        /*builder.setIcon(R.drawable.desafiosdeti);*/
+        //define um botão positivo
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+            }
+        });
+        //define um botão negativo
+        /*builder.setNegativeButton("Negativo", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(MainActivity.this, "Vamos Melhorar!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //define um botão neutro
+        builder.setNeutralButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(MainActivity.this, "Nem Sim, nem Não!", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+        //cria o AlertDialog
+        AlertDialog  alerta = builder.create();
+        //Exibe
+        alerta.show();
+        Button positiveButton = alerta.getButton(AlertDialog.BUTTON_POSITIVE);
+        LinearLayout parent = (LinearLayout) positiveButton.getParent();
+        parent.setGravity(Gravity.CENTER_HORIZONTAL);
+        View leftSpacer = parent.getChildAt(1);
+        leftSpacer.setVisibility(View.GONE);
     }
 }
