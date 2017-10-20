ee7c9614b5066eb1b9ee7401e9bdc92617e9894e
eliasdourado <32002044+eliasdourado@users.noreply.github.com>
2017-10-20 01:35:57 -0300

    CadastroUsuário
    Inclusão da tela de cadastro do usuário

---
+++ EASYBALADS/app/src/main/java/easybalads/easybalads/CadastroUsuario.java
@@ -0,0 +1,13 @@
+package easybalads.easybalads;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CadastroUsuario extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario);
+    }
+}
